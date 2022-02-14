3f6f933daf2aa8950760f6cc686333faf70582f3
camilo <casfisica@gmail.com>
2022-02-14 17:11:50 -0500

    list

--- FreshMvvmSQLiteExporImportToExel/FreshMvvmSQLiteExporImportToExel/PageModels/ListPageModel.cs
+++ FreshMvvmSQLiteExporImportToExel/FreshMvvmSQLiteExporImportToExel/PageModels/ListPageModel.cs
@@ -1,14 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows.Input;
 using FreshMvvm;
+using FreshMvvmSQLiteExporImportToExel.Model;
+using FreshMvvmSQLiteExporImportToExel.Services;
 using Xamarin.Forms;
 
 namespace FreshMvvmSQLiteExporImportToExel.PageModels
 {
     internal class ListPageModel : FreshBasePageModel
     {
+
+        private Repository _repository = FreshIOC.Container.Resolve<Repository>();
+        private User _selectedUser = null;
+
+        /// <summary>
+        /// Una colleccion para mostrar los usuarios en las list view
+        /// </summary>
+        public ObservableCollection<User> Users { get; private set; }
+
+        /// <summary>
+        /// Para ligarla a la propiedad SelectedItem, 
+        /// para llamar el commando EditUserCommand 
+        /// </summary>
+        public User SelectedUser
+        {
+            get { return _selectedUser; }
+            set
+            {
+                _selectedUser = value;
+                if (value != null) EditUserCommand.Execute(value);
+            }
+        }
+
+        /// <summary>
+        /// Command associated with the edit item action.
+        /// Navigates to the ItemPageModel with the selected item as the Init object.
+        /// </summary>
+        public ICommand EditUserCommand
+        {
+            get
+            {
+                return new Command(async (item) => {
+                    await CoreMethods.PushPageModel<AddUserPageModel>(item);
+                });
+            }
+        }
 
     }
 }
